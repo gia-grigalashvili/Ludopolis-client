7f6b1f5f4d52d656b63db54c4954dcad9aaa6285
gia-grigalashvili <grigalagia07@gmail.com>
2025-07-24 19:22:37 +0400

    add style.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import "./styles.css";
 // Import the generated route tree
 import { getCurrentUser } from "./services/auth";
 
