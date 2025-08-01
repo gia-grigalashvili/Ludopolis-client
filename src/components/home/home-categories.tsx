f7e6a017224dee66feeac6821cea253baa7e45c5
gia-grigalashvili <grigalagia07@gmail.com>
2025-08-01 13:31:27 +0400

    pnpm add

--- src/components/home/home-categories.tsx
+++ src/components/home/home-categories.tsx
@@ -52,6 +52,7 @@ export function HomeCategories() {
               <Link
                 key={`${category.id}-${index}`}
                 to={`/games`}
+                search={{ categories: [category.id] }}
                 className="group cursor-pointer cursor-target"
               >
                 <div className="bg-black/30 backdrop-blur-sm border-2 border-purple-500/50 p-6 text-center transition-all hover:border-purple-400 hover:bg-purple-600/20">
